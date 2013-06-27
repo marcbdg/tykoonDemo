584dffc3af6bdfdbe241b7ace9675af955dca705
Brian Hagan <bbhagan@gmail.com>
2013-06-27 15:59:30 -0700

    fixed child signature call

--- js/tykoon.js
+++ js/tykoon.js
@@ -33,7 +33,7 @@ var transitionToStartTasks = function(e) {
    // -1 on month, apparently 0-based for months
    var birthday = new Date(parseInt(birthdayString.substring(0,4)), parseInt(birthdayString.substring(5,7))-1, parseInt(birthdayString.substring(8)));
    var gender = $('#getStartedFormChildGender').val();
-   var child = new Child(0, name, '', '', birthday, 0, gender);
+   var child = new Child(0, name, '', '', '', birthday, 0, gender);
    tykoonData.parent.children.push(child);
 
    //populate page with name
@@ -43,6 +43,11 @@ var transitionToStartTasks = function(e) {
    for(var i in cannedTasks.tasks) {
       if (cannedTasks.tasks.hasOwnProperty(i)) {
          var task = cannedTasks.tasks(i);
+
+         alert('task.gender: ' + task.gender);
+         alert('task.age.min: ' + task.ageRange.min);
+         alert('task.age.max: ' + task.ageRange.max);
+
          if ((task.gender == b || task.gender == child.gender) && (child.age >= task.ageRange.min && child.age <= task.ageRange.max)) {
             var html = '<div class="taskItem"><h4>' + task.name + '</h4><p>' + task.numPeople + ' kids are doing this</p></div>';
             $('#startTasks .taskCatalog').append(html);
