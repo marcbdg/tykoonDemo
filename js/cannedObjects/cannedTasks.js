a98bb684b73b64d7badcd2cf7940d7cbbc75681d
Marc Greenberg <marcbdg@gmail.com>
2013-06-28 16:15:19 -0700

    more tasks data

--- js/cannedObjects/cannedTasks.js
+++ js/cannedObjects/cannedTasks.js
@@ -58,5 +58,178 @@ var cannedTasks = {
          gender: 'b',
          numPeople: 874
       },
+      
+      {
+         id:  5,
+         name: 'Fold the laundry',
+         ageRange: {
+            min: 8,
+            max: 18
+         },
+         gender: 'f',
+         numPeople: 100
+       },
+       {
+          id: 6,
+         name: 'Pick up your toys',
+         ageRange: {
+            min: 3,
+            max: 9
+         },
+         gender: 'b',
+         numPeople: 100
+    },
+    {
+       id: 7,
+         name: 'Empty the dishwasher',
+         ageRange: {
+            min: 8,
+            max: 18
+         },
+         gender: 'b',
+         numPeople: 100
+    },
+    {
+       id: 8,
+         name: 'Rake the leaves',
+         ageRange: {
+            min: 10,
+            max: 18
+         },
+         gender: 'm',
+         numPeople: 100
+    },
+    {
+       id: 9,
+         name: 'Vacuum',
+         ageRange: {
+            min: 12,
+            max: 18
+         },
+         gender: 'f',
+         numPeople: 100
+    },
+    {
+       id: 10,
+         name: 'Dry the dishes',
+         ageRange: {
+            min: 10,
+            max: 18
+         },
+         gender: 'b',
+         numPeople: 100
+    },
+    {
+       id: 11,
+         name: 'Take out the trash',
+         ageRange: {
+            min: 8,
+            max: 18
+         },
+         gender: 'm',
+         numPeople: 100
+    },
+    {
+       id: 12,
+         name: 'Take out the recycling',
+         ageRange: {
+            min: 8,
+            max: 18
+         },
+         gender: 'b',
+         numPeople: 100
+    },
+    {
+       id: 13,
+         name: 'Wash your hands',
+         ageRange: {
+            min: 5,
+            max: 10
+         },
+         gender: 'b',
+         numPeople: 100
+    },
+    {
+       id: 14,
+         name: 'Wash your face',
+         ageRange: {
+            min: 5,
+            max: 12
+         },
+         gender: 'b',
+         numPeople: 100
+    },
+    {
+       id: 15,
+         name: 'Take a bath',
+         ageRange: {
+            min: 3,
+            max: 7
+         },
+         gender: 'b',
+         numPeople: 100
+    },
+    {
+       id: 16,
+         name: 'Take a shower',
+         ageRange: {
+            min: 8,
+            max: 18
+         },
+         gender: 'b',
+         numPeople: 100
+    },
+    {
+       id: 17,
+         name: 'Do your homework',
+         ageRange: {
+            min: 8,
+            max: 18
+         },
+         gender: 'b',
+         numPeople: 100
+    },
+    {
+       id: 18,
+         name: 'Do your book report',
+         ageRange: {
+            min: 8,
+            max: 18
+         },
+         gender: 'b',
+         numPeople: 100
+    },
+    {
+       id: 19,
+         name: 'Feed the dog',
+         ageRange: {
+            min: 6,
+            max: 18
+         },
+         gender: 'b',
+         numPeople: 100
+    },
+    {
+       id: 20,
+         name: 'Feed the cat',
+         ageRange: {
+            min: 6,
+            max: 18
+         },
+         gender: 'b',
+         numPeople: 100
+    },
+    {
+       id: 21,
+         name: 'Clean the dog run',
+         ageRange: {
+            min: 8,
+            max: 18
+         },
+         gender: 'b',
+         numPeople: 100
+    }      
+      
+      
    ]
 };
